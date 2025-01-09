9b83e307e7ff659f5378baaeb1e437351abeb32b
Malik-Zohab-Debug <zohaibzahid112233@gmail.com>
2025-01-10 00:13:53 +0500

    integrated recipe search api

--- src/components/recipeSearchBar/RecipeSearchBar.jsx
+++ src/components/recipeSearchBar/RecipeSearchBar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./recipeSearchBar.css";
 
 const RecipeSearchBar = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [keyword, setKeyword] = useState("");
+
+  const getSearchedRecipes = (recipe) => {
+    fetch(`https://dummyjson.com/recipes/search?q=${recipe}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className="recipeSearchControl">
       <input
         type="text"
         placeholder="Search your favourite recipe"
         className="searchBar"
+        onChange={(e) => setKeyword(e.target.value)}
       />
-      <button className="recipeSearchBtn">Search Your Recipe</button>
+      <button
+        className="recipeSearchBtn"
+        onClick={() => getSearchedRecipes(keyword)}
+      >
+        Search Your Recipe
+      </button>
     </div>
   );
 };
